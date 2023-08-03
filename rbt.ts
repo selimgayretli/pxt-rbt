8b3b30485319b02e4da24df1721adf14a7cf7054
selimgayretli <selim.gayretli@robotistan.com>
2023-08-03 15:22:58 +0300

    changing names to gator:moisture

--- rbt.ts
+++ rbt.ts
@@ -170,7 +170,7 @@ namespace rbt {
     /**
      * turn on LCD backlight
      */
-    //% blockId="I2C_LCD1620_BACKLIGHT_ON" block="turn on backlight"
+    //% blockId="I2C_LCD1620_BACKLIGHT_ON" block="isik ac"
     //% weight=71 blockGap=8
     //% parts=LCD1602_I2C trackArgs=0
     export function BacklightOn(): void {
