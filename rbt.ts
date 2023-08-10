e68b69c9a53e8d32ed6f8af2f8fbd7d774ccf136
selimgayretli <selim.gayretli@robotistan.com>
2023-08-10 10:31:01 +0300

    denene

--- rbt.ts
+++ rbt.ts
@@ -3,57 +3,7 @@
 * From microbit/micropython Chinese community.
 * http://www.micropython.org.cn
 */
-  const APDS9960_RAM = 0x00
-  const APDS9960_ENABLE = 0x80
-  const APDS9960_ATIME = 0x81
-  const APDS9960_WTIME = 0x83
-  const APDS9960_AILTIL = 0x84
-  const APDS9960_AILTH = 0x85
-  const APDS9960_AIHTL = 0x86
-  const APDS9960_AIHTH = 0x87
-  const APDS9960_PILT = 0x89
-  const APDS9960_PIHT = 0x8B
-  const APDS9960_PERS = 0x8C
-  const APDS9960_CONFIG1 = 0x8D
-  const APDS9960_PPULSE = 0x8E
-  const APDS9960_CONTROL = 0x8F
-  const APDS9960_CONFIG2 = 0x90
-  const APDS9960_ID = 0x92
-  const APDS9960_STATUS = 0x93
-  const APDS9960_CDATAL = 0x94
-  const APDS9960_CDATAH = 0x95
-  const APDS9960_RDATAL = 0x96
-  const APDS9960_RDATAH = 0x97
-  const APDS9960_GDATAL = 0x98
-  const APDS9960_GDATAH = 0x99
-  const APDS9960_BDATAL = 0x9A
-  const APDS9960_BDATAH = 0x9B
-  const APDS9960_PDATA = 0x9C
-  const APDS9960_POFFSET_UR = 0x9D
-  const APDS9960_POFFSET_DL = 0x9E
-  const APDS9960_CONFIG3 = 0x9F
-  const APDS9960_GPENTH = 0xA0
-  const APDS9960_GEXTH = 0xA1
-  const APDS9960_GCONF1 = 0xA2
-  const APDS9960_GCONF2 = 0xA3
-  const APDS9960_GOFFSET_U = 0xA4
-  const APDS9960_GOFFSET_D = 0xA5
-  const APDS9960_GOFFSET_L = 0xA7
-  const APDS9960_GOFFSET_R = 0xA9
-  const APDS9960_GPULSE = 0xA6
-  const APDS9960_GCONF3 = 0xAA
-  const APDS9960_GCONF4 = 0xAB
-  const APDS9960_GFLVL = 0xAE
-  const APDS9960_GSTATUS = 0xAF
-  const APDS9960_IFORCE = 0xE4
-  const APDS9960_PICLEAR = 0xE5
-  const APDS9960_CICLEAR = 0xE6
-  const APDS9960_AICLEAR = 0xE7
-  const APDS9960_GFIFO_U = 0xFC
-  const APDS9960_GFIFO_D = 0xFD
-  const APDS9960_GFIFO_L = 0xFE
-  const APDS9960_GFIFO_R = 0xFF
-  const APDS9960_I2C_ADDRESS = 0x39
+
 /**
  * Custom blocks
  */
@@ -62,9 +12,6 @@ namespace APDS9960 {
     let i2cAddr: number // 0x3F: PCF8574A, 0x27: PCF8574
     let BK: number      // backlight control
     let RS: number      // command/data
-	
-	let _wbuf = pins.createBuffer(2);
-	
     /**
     * Unit of Ultrasound Module
     */
@@ -76,6 +23,7 @@ namespace APDS9960 {
     }
 
     export enum Direction {
+        
         //% block="up"
         Up,
         //% block="down"
@@ -84,46 +32,156 @@ namespace APDS9960 {
         Left,
         //% block="right"
         Right,
+    
     }
     
     export enum RGBcolor {
+        
         //% block="Red"
         Red,
         //% block="Green"
         Green,
         //% block="Blue"
         Blue,
+        
+    
     }
-	
-    function writeReg(reg: number, d: number): void {
-        pins.i2cWriteNumber(i2cAddr, ((reg << 8) + d), NumberFormat.UInt16BE, false)
-    }
-	
-    function getReg(reg: number): number {
-        pins.i2cWriteNumber(APDS9960_I2C_ADDRESS, reg, NumberFormat.UInt8BE);
-		let x = pins.i2cReadNumber(APDS9960_I2C_ADDRESS, NumberFormat.UInt8BE);
-        return x;
-    }
-	
-    function get2Reg(reg: number): number {
-        pins.i2cWriteNumber(APDS9960_I2C_ADDRESS, reg, NumberFormat.UInt8BE);
-        return pins.i2cReadNumber(APDS9960_I2C_ADDRESS, NumberFormat.UInt16LE);
-    }
-	
-	
-	/**
-    *Change the code (Atakan) 
-    * Cars can extend the ultrasonic function to prevent collisions and other functions.. 
-    * @param ldr two states of ultrasonic module, eg: Centimeters
-    */
-    //% blockId=ldr block="APDS-9960 Light Value4444"
-    //% weight=35
-    export function ldr(): number { 
-		writeReg(APDS9960_ENABLE, 0x7F);
-		while(!colorDataReady());
-		let c = get2Reg(APDS9960_CDATAL);
-        return c;
 
+    // set LCD reg
+    function setreg(d: number) {
+        pins.i2cWriteNumber(i2cAddr, d, NumberFormat.Int8LE)
+        basic.pause(1)
+    }
+
+    // send data to I2C bus
+    function set(d: number) {
+        d = d & 0xF0
+        d = d + BK + RS
+        setreg(d)
+        setreg(d + 4)
+        setreg(d)
+    }
+
+    // send command
+    function cmd(d: number) {
+        RS = 0
+        set(d)
+        set(d << 4)
+    }
+
+    // send data
+    function dat(d: number) {
+        RS = 1
+        set(d)
+        set(d << 4)
+    }
+
+    // auto get LCD address
+    function AutoAddr() {
+        let k = true
+        let addr = 0x20
+        let d1 = 0, d2 = 0
+        while (k && (addr < 0x28)) {
+            pins.i2cWriteNumber(addr, -1, NumberFormat.Int32LE)
+            d1 = pins.i2cReadNumber(addr, NumberFormat.Int8LE) % 16
+            pins.i2cWriteNumber(addr, 0, NumberFormat.Int16LE)
+            d2 = pins.i2cReadNumber(addr, NumberFormat.Int8LE)
+            if ((d1 == 7) && (d2 == 0)) k = false
+            else addr += 1
+        }
+        if (!k) return addr
+
+        addr = 0x38
+        while (k && (addr < 0x40)) {
+            pins.i2cWriteNumber(addr, -1, NumberFormat.Int32LE)
+            d1 = pins.i2cReadNumber(addr, NumberFormat.Int8LE) % 16
+            pins.i2cWriteNumber(addr, 0, NumberFormat.Int16LE)
+            d2 = pins.i2cReadNumber(addr, NumberFormat.Int8LE)
+            if ((d1 == 7) && (d2 == 0)) k = false
+            else addr += 1
+        }
+        if (!k) return addr
+        else return 0
+
+    }
+
+    /**
+     * initial LCD, set I2C address. Address is 39/63 for PCF8574/PCF8574A
+     * @param Addr is i2c address for LCD, eg: 0, 39, 63. 0 is auto find address
+     */
+    //% blockId="I2C_LCD1620_SET_ADDRESS" block="APDS-9960 initialize with Address %addr"
+    //% weight=100 blockGap=8
+    //% parts=LCD1602_I2C trackArgs=0
+    export function LcdInit(Addr: number) {
+        if (Addr == 0) i2cAddr = AutoAddr()
+        else i2cAddr = Addr
+        BK = 8
+        RS = 0
+        cmd(0x33)       // set 4bit mode
+        basic.pause(5)
+        set(0x30)
+        basic.pause(5)
+        set(0x20)
+        basic.pause(5)
+        cmd(0x28)       // set mode
+        cmd(0x0C)
+        cmd(0x06)
+        cmd(0x01)       // clear
+    }
+
+
+    /**
+    * Cars can extend the ultrasonic function to prevent collisions and other functions.. 
+    * @param Sonarunit two states of ultrasonic module, eg: Centimeters
+    */
+    //% blockId=ultrasonic block="APDS-9960 Distance %unit"
+    //% weight=35
+    export function ultrasonic(unit: SonarUnit, maxCmDistance = 500): number {
+        // send pulse
+        pins.setPull(DigitalPin.P8, PinPullMode.PullNone);
+        pins.digitalWritePin(DigitalPin.P8, 0);
+        control.waitMicros(2);
+        pins.digitalWritePin(DigitalPin.P8, 1);
+        control.waitMicros(10);
+        pins.digitalWritePin(DigitalPin.P8, 0);
+        // read pulse
+        const d = pins.pulseIn(DigitalPin.P12, PulseValue.High, maxCmDistance * 50);
+        switch (unit) {
+            case SonarUnit.Centimeters:
+                return Math.floor(d * 34 / 2 / 1000);
+            case SonarUnit.Inches:
+                return Math.floor(d * 34 / 2 / 1000 * 0.3937);
+            default:
+                return d;
+        }
+    }
+
+
+    /**
+     * TODO: Direction Value
+     */
+    //% block="Movement in the %state direction"
+    //% weight=15
+    export function directionkeys(state: Direction): boolean {
+        pins.setPull(DigitalPin.P13, PinPullMode.PullNone)
+        pins.setPull(DigitalPin.P14, PinPullMode.PullNone)
+        let left_tracking = pins.digitalReadPin(DigitalPin.P13);
+        let right_tracking = pins.digitalReadPin(DigitalPin.P14);
+        if (left_tracking == 0 && right_tracking == 0 && state == 0) {
+            return true;
+        }
+        else if (left_tracking == 1 && right_tracking == 0 && state == 1) {
+            return true;
+        }
+        else if (left_tracking == 0 && right_tracking == 1 && state == 2) {
+            return true;
+        }
+        else if (left_tracking == 1 && right_tracking == 1 && state == 3) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
      /**
@@ -132,138 +190,45 @@ namespace APDS9960 {
     //% block="APDS-9960 RGB Color %colorgb"
     //% weight=15
     export function rgbColor(colorgb: RGBcolor): boolean {
-			return true;
-    }
-
-    /**
-    * Cars can extend the ultrasonic function to prevent collisions and other functions.. 
-    * @param Sonarunit two states of ultrasonic module, eg: Centimeters
-    */
-    //% blockId=ultrasonic block="APDS-9960 Distanceaa %unit"
-    //% weight=35
-    export function ultrasonic(unit: SonarUnit, maxCmDistance = 500): number {
-       let x = getReg(APDS9960_ID)
-	   return x;
-    }
-	
-	    /**
-     * TODO: Direction Value
-     */
-    //% block="Movement in the %state direction"
-    //% weight=15
-    export function directionkeys(state: Direction): boolean {
+        pins.setPull(DigitalPin.P13, PinPullMode.PullNone)
+        pins.setPull(DigitalPin.P14, PinPullMode.PullNone)
+        let left_tracking = pins.digitalReadPin(DigitalPin.P13);
+        let right_tracking = pins.digitalReadPin(DigitalPin.P14);
+        if (left_tracking == 0 && right_tracking == 0 && colorgb == 0) {
             return true;
-
-
+        }
+        else if (left_tracking == 1 && right_tracking == 0 && colorgb == 1) {
+            return true;
+        }
+        else if (left_tracking == 0 && right_tracking == 1 && colorgb == 2) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
-
-    /**
-     * initial LCD, set I2C address. Address is 39/63 for PCF8574/PCF8574A
-     * @param Addr is i2c address for LCD, eg: 0, 39, 63. 0 is auto find address
-     */
-    //% blockId="init" block="APDS-9960 initialize %addr"
-    //% weight=100 blockGap=8
-    //% parts=LCD1602_I2C trackArgs=0
-    export function init(Addr: number) {
-	  setADCIntegrationTime();
-	  setADCGain();
-	  
-	  enableGesture();
-	  enableProximity();
-	  enableColor();
-  
-	  disableColorInterrupt();
-	  disableProximityInterrupt();
-	  clearInterrupt();
-  
-	  enable_true();
-  
-      setGestureDimensions();
-	  setGestureFIFOThreshold();
-	  setGestureGain();
-	  setGestureProximityThreshold();
-
-	  writeReg(APDS9960_GPULSE, 0x83);
-    }
-	
-	function setADCIntegrationTime(): void {
-      let temp;
-
-	  temp = 10;
-	  temp /= 2.78;
-	  temp = 256 - temp;
-	  if (temp > 255)
-		temp = 255;
-	  if (temp < 0)
-		temp = 0;
-
-	  writeReg(APDS9960_ATIME, temp);
-    }
-	
-	function setADCGain(): void {
-	  writeReg(APDS9960_CONTROL, 1);
-	}
-	
-	function enableGesture(): void {
-	  writeReg(APDS9960_GCONF4, 0);
-	}
-	
-	function enableProximity(): void {
-	  writeReg(APDS9960_ENABLE, 0x7B);
-	}
-	
-	function enableColor(): void {
-	  writeReg(APDS9960_ENABLE, 0x7D);
-	}
-	
-	function disableColorInterrupt(): void {
-	  writeReg(APDS9960_ENABLE, 0x6F);
-	}
-	
-	function disableProximityInterrupt(): void {
-	  writeReg(APDS9960_ENABLE, 0x5F);
-	}
-	
-	function clearInterrupt(): void {
-	  writeReg(APDS9960_AICLEAR, 0);
-	}
-	
-	function enable_false(): void {
-	  writeReg(APDS9960_ENABLE, 0x7E);
-	}
-	
-	function enable_true(): void {
-	  writeReg(APDS9960_ENABLE, 0x7F);
-	}
-	
-	function setGestureDimensions(): void {
-	  writeReg(APDS9960_GCONF3, 0);
-	}
-
-	function setGestureFIFOThreshold(): void {
-	  writeReg(APDS9960_GCONF1, 0x52);
-	}
-	
-	function setGestureGain(): void {
-	  writeReg(APDS9960_GCONF2, 0x53);
-	}
-
-	function setGestureProximityThreshold(): void {
-	  writeReg(APDS9960_GPENTH, 0x32);
-	}
-	
-	function colorDataReady(): number {
-		let cdata;
-		  cdata = getReg(APDS9960_STATUS);
-		  return cdata;
-	}
-
-
-
-
     
 
+    /**
+    *Change the code (Atakan) 
+    * Cars can extend the ultrasonic function to prevent collisions and other functions.. 
+    * @param ldr two states of ultrasonic module, eg: Centimeters
+    */
+    //% blockId=ldr block="APDS-9960 Light Value"
+    //% weight=35
+    export function ldr(): number { 
+        // send pulse
+        pins.setPull(DigitalPin.P8, PinPullMode.PullNone);
+        pins.digitalWritePin(DigitalPin.P8, 0);
+        control.waitMicros(2);
+        pins.digitalWritePin(DigitalPin.P8, 1);
+        control.waitMicros(10);
+        pins.digitalWritePin(DigitalPin.P8, 0);
+        // read pulse
 
+        return 0
+
+    }
 
 
      
